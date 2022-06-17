6a83e81ab5590812453b2d0b44e7f66f93c1b7b2
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-17 12:20:33 +0530

    :zap: setup ejs to be render on get / route

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
 
-const PORT = process.env.PORT || 9008
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('room');
+});
+
+const PORT = process.env.PORT || 9008;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
