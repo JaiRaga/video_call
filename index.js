f888d89759c40ce1adbaab90e8d4ac2927571281
JaiRaga <raga.j.santhosh@gmail.com>
2022-06-19 15:51:47 +0530

    :zap: receives the message from the client and forwards it to the chat room

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const peerServer = ExpressPeerServer(server, { debug: true });
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.use("/peerjs", peerServer)
+app.use('/peerjs', peerServer);
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -26,6 +26,9 @@ io.on('connection', (socket) => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).emit('user-connected', userId);
+    socket.on('message', (message) => {
+      io.to(roomId).emit('createMessage', message);
+    });
   });
 });
 
